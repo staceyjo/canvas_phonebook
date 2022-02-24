15c0d065b627a97b554759683f8cf8550fabf983
StaceyJo <stacey.l.joseph@gmail.com>
2022-02-24 12:58:41 -0500

    Adds menu list to every page within the phonebook

--- phonebook.py
+++ phonebook.py
@@ -1,9 +1,11 @@
-menu ="""
+menu = """
 Electronic Phone Book
 =====================
 1. Look up an entry
 2. Set an entry
 3. Delete an entry
 4. List all entries
-5. Quit
-""
+5. Quit)
+"""
+
+print (menu)
